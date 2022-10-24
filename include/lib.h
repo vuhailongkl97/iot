aa91ae460080229641273571395d514366dbf7bc
longkl <vuhailongkl97@gmail.com>
2022-10-24 17:46:17 +0700

    add listener for on data detection

--- include/lib.h
+++ include/lib.h
@@ -1,14 +1,70 @@
-#include "yolo_v2_class.hpp"
+#pragma once
+#include <vector>
+#include <chrono>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <cmath>
+
+#ifdef OPENCV
+#include <opencv2/opencv.hpp>          // C++
+#include <opencv2/highgui/highgui_c.h> // C
+#include <opencv2/imgproc/imgproc_c.h> // C
+#endif
 #include <fstream>
 
-void show_console_result(std::vector<bbox_t> const result_vec,
-                         std::vector<std::string> const obj_names,
-                         int frame_id = -1);
+struct obj_t
+{
+    float prob;                  // confidence - probability that the object was found correctly
+    unsigned int obj_id;         // class of object - from range [0, classes-1]
+    unsigned int track_id;       // tracking id for video (0 - untracked, 1 - inf - tracked object)
+    unsigned int frames_counter; // counter of frames on which the object was detected
+};
 
-std::vector<std::string> objects_names_from_file(std::string const filename);
-void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec,
-                std::vector<std::string> obj_names, int current_det_fps = -1,
-                int current_cap_fps = -1, float thresh = 0.2);
+struct DataResult
+{
+    cv::Mat f;
+    std::vector<obj_t> s;
+    std::vector<std::string> d;
+    uint64_t frameid;
+};
 
-float getMedian(std::vector<float>& v);
-std::vector<bbox_t> get_3d_coordinates(std::vector<bbox_t> bbox_vect, cv::Mat xyzrgba);
+class Notifier
+{
+public:
+    void work(const DataResult& d)
+    {
+        float avg_prob = prepare(d);
+        doNotify(tmpImgFilePath, avg_prob);
+    }
+    virtual void doNotify(const char* pathToFrame, float avg_prob) = 0;
+    float prepare(const DataResult& d)
+    {
+        //std::cout << "saving the image \n";
+        return 0;
+    }
+
+private:
+    const char* tmpImgFilePath = "/tmp/img.png";
+};
+
+class consoleNotifier : public Notifier
+{
+public:
+    void doNotify(const char* pathToFrame, float avg_prob);
+};
+
+class DataUpdateListener
+{
+public:
+    void addSubscriber(Notifier* nf) { m_nfers.emplace_back(nf); }
+    void onDataUpdate(DataResult& data)
+    {
+        for (auto& nf : m_nfers) {
+            nf->work(data);
+        }
+    }
+
+private:
+    std::vector<std::unique_ptr<Notifier>> m_nfers;
+};
--- src/yolo_console_dll.cpp
+++ src/yolo_console_dll.cpp
@@ -59,41 +59,6 @@
 
 #endif // OPENCV
 
-template<typename T, typename U>
-struct DataResult
-{
-    std::vector<T> s;
-    std::vector<U> d;
-    uint64_t frameid;
-};
-
-template<typename T>
-class DataUpdateListener
-{
-public:
-    using callback_function = void (*)(const T& data);
-    void setCallback(callback_function cb) { callback_list.push_back(cb); }
-    void onDataUpdate(const T& data)
-    {
-        for (auto cb : callback_list) {
-            cb(data);
-        }
-    }
-
-private:
-    std::vector<callback_function> callback_list;
-};
-
-void mqtt_callback(const DataResult<int, int>& data)
-{
-    std::cout << "mqtt_callback" << std::endl;
-}
-
-void console_callback(const DataResult<bbox_t, std::string>& data)
-{
-    show_console_result(data.s, data.d, data.frameid);
-}
-
 template<typename T>
 class send_one_replaceable_object_t
 {
@@ -145,13 +110,113 @@ struct detection_data_t
       new_detection(false), exit_flag(false) {}
 };
 
+void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec,
+                std::vector<std::string> obj_names, int current_det_fps,
+                int current_cap_fps, float thresh)
+{
+    // int const colors[6][3] = { { 1,0,1 },{ 0,0,1 },{ 0,1,1 },{ 0,1,0 },{
+    // 1,1,0 },{ 1,0,0 } };
+
+    for (auto& i : result_vec) {
+        cv::Scalar color = obj_id_to_color(i.obj_id);
+        cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 2);
+        if (obj_names.size() > i.obj_id) {
+            std::string obj_name = obj_names[i.obj_id];
+            if (i.track_id > 0)
+                obj_name += " - " + std::to_string(i.track_id);
+            cv::Size const text_size = getTextSize(
+              obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2,
+              0);
+            int max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
+            max_width = std::max(max_width, (int)i.w + 2);
+            // max_width = std::max(max_width, 283);
+            std::string coords_3d;
+            if (!std::isnan(i.z_3d)) {
+                std::stringstream ss;
+                ss << std::fixed << std::setprecision(2)
+                   << "x:" << i.x_3d << "m y:" << i.y_3d
+                   << "m z:" << i.z_3d << "m ";
+                coords_3d = ss.str();
+                cv::Size const text_size_3d = getTextSize(
+                  ss.str(), cv::FONT_HERSHEY_COMPLEX_SMALL,
+                  0.8, 1, 0);
+                int const max_width_3d =
+                  (text_size_3d.width > i.w + 2) ? text_size_3d.width : (i.w + 2);
+                if (max_width_3d > max_width)
+                    max_width = max_width_3d;
+            }
+
+            cv::rectangle(
+              mat_img,
+              cv::Point2f(std::max((int)i.x - 1, 0),
+                          std::max((int)i.y - 35, 0)),
+              cv::Point2f(std::min((int)i.x + max_width,
+                                   mat_img.cols - 1),
+                          std::min((int)i.y, mat_img.rows - 1)),
+              color, CV_FILLED, 8, 0);
+            putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 16),
+                    cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2,
+                    cv::Scalar(0, 0, 0), 2);
+            if (!coords_3d.empty())
+                putText(mat_img, coords_3d,
+                        cv::Point2f(i.x, i.y - 1),
+                        cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8,
+                        cv::Scalar(0, 0, 0), 1);
+        }
+    }
+    if (current_det_fps >= 0 && current_cap_fps >= 0) {
+        std::string fps_str =
+          "FPS detection: " + std::to_string(current_det_fps) +
+          " FPS capture: " + std::to_string(current_cap_fps) +
+          " " + std::to_string(thresh);
+        putText(mat_img, fps_str, cv::Point2f(10, 20),
+                cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2,
+                cv::Scalar(50, 255, 0), 1);
+    }
+}
+
+std::vector<bbox_t> get_3d_coordinates(std::vector<bbox_t> bbox_vect,
+                                       cv::Mat xyzrgba)
+{
+    return bbox_vect;
+}
+
+std::vector<std::string> objects_names_from_file(std::string const filename)
+{
+    std::ifstream file(filename);
+    std::vector<std::string> file_lines;
+    if (!file.is_open()) return file_lines;
+    for (std::string line; getline(file, line);)
+        file_lines.push_back(line);
+    std::cout << "object names loaded \n";
+    return file_lines;
+}
+
+std::vector<obj_t> convertBbox2obj(const std::vector<bbox_t>& v)
+{
+    std::vector<obj_t> ret;
+    for (auto& elem : v) {
+        ret.push_back({elem.prob, elem.obj_id, elem.track_id, elem.frames_counter});
+    }
+    return ret;
+}
+
+
+float getMedian(std::vector<float>& v)
+{
+    size_t n = v.size() / 2;
+    std::nth_element(v.begin(), v.begin() + n, v.end());
+    return v[n];
+}
+
 int main(int argc, char* argv[])
 {
     std::string names_file = "data/coco.names";
     std::string cfg_file = "cfg/yolov4-tiny.cfg";
     std::string weights_file = "data/yolov4-tiny.weights";
     std::string filename;
-    DataUpdateListener<DataResult<bbox_t, std::string>> listener;
+    DataUpdateListener listener;
+    listener.addSubscriber(new consoleNotifier());
 
     if (argc > 4) { // voc.names yolo-voc.cfg yolo-voc.weights test.mp4
         names_file = argv[1];
@@ -174,7 +239,6 @@ int main(int argc, char* argv[])
     auto server = std::thread(runServer, std::ref(thresh));
     server.detach();
 
-    //listener.setCallback(console_callback);
     while (true) {
         if (filename.size() == 0) {
             std::cout << "file name is empty, this program is exitting" << std::endl;
@@ -374,8 +438,9 @@ int main(int argc, char* argv[])
                         draw_boxes(draw_frame, result_vec, obj_names, current_fps_det,
                                    current_fps_cap, thresh);
 
-                        DataResult<bbox_t, std::string> data_result{
-                          result_vec,
+                        DataResult data_result{
+                          draw_frame,
+                          convertBbox2obj(result_vec),
                           obj_names,
                           detection_data.frame_id,
                         };
