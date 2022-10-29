680e5339de72c82b1cb522c472d303867c13f8e0
long.vu <vuhailongkl97@gmail.com>
2022-10-29 09:56:05 +0700

    update clang-format 80 chars each row

--- src/lib.cpp
+++ src/lib.cpp
@@ -15,24 +15,26 @@ void consoleNotifier::doWork(const filteredDataResult& d)
 
 void discordNotifier::doWork(const filteredDataResult& d)
 {
-	auto curTime = std::time(nullptr);
-	if (curTime < time4rest)
-		return;
+    auto curTime = std::time(nullptr);
+    if (curTime < time4rest) return;
 
-	std::pair<obj_t, std::time_t> p = std::make_pair(d.objs[0], curTime);
-	recent_results.push_front(p);
+    std::pair<obj_t, std::time_t> p = std::make_pair(d.objs[0], curTime);
+    recent_results.push_front(p);
 
-	if (recent_results.size() >= QUEUE_ENTRY_LIMIT) {
-		// main checking
-		auto lastOneTime = recent_results.back().second;
-		if (curTime - lastOneTime <= TIME_LIMIT) {
-			cv::imwrite(tmpImgPath, d.frame);
-			time4rest = curTime + TIME_SKIP;
-			recent_results.clear();
-			MLogger::getLoggerInstance()->info("wrote an image, start skiping for {} secs ", TIME_SKIP);
-			system("/usr/bin/curl http://localhost:1234/updated -X POST -d \"/tmp/img.png\" --max-time 2 ");
-		} else {
-			recent_results.pop_back();
-		}
-	}
+    if (recent_results.size() >= QUEUE_ENTRY_LIMIT) {
+        // main checking
+        auto lastOneTime = recent_results.back().second;
+        if (curTime - lastOneTime <= TIME_LIMIT) {
+            cv::imwrite(tmpImgPath, d.frame);
+            time4rest = curTime + TIME_SKIP;
+            recent_results.clear();
+            MLogger::getLoggerInstance()->info(
+              "wrote an image, start skiping for {} secs ", TIME_SKIP);
+            system("/usr/bin/curl http://localhost:1234/updated -X POST -d "
+                   "\"/tmp/img.png\" --max-time 2 &>/dev/null");
+        }
+        else {
+            recent_results.pop_back();
+        }
+    }
 }
