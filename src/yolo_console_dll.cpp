2eec6457f074eda1de04456f4fa7c3da8f0fcebd
longkl <vuhailongkl97@gmail.com>
2022-10-31 10:09:13 +0700

    draw person only

--- src/yolo_console_dll.cpp
+++ src/yolo_console_dll.cpp
@@ -114,38 +114,21 @@ void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec,
                 std::vector<std::string> obj_names, int current_det_fps,
                 int current_cap_fps, float thresh)
 {
-    // int const colors[6][3] = { { 1,0,1 },{ 0,0,1 },{ 0,1,1 },{ 0,1,0 },{
-    // 1,1,0 },{ 1,0,0 } };
-
     for (auto& i : result_vec) {
         cv::Scalar color = obj_id_to_color(i.obj_id);
-        cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 2);
         if (obj_names.size() > i.obj_id) {
             std::string obj_name = obj_names[i.obj_id];
+			if (obj_name != std::string("person")) {
+				continue;
+			}
+        	cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 1);
             if (i.track_id > 0)
-                obj_name += " - " + std::to_string(i.track_id);
+                obj_name = std::to_string(i.track_id);
             cv::Size const text_size = getTextSize(
-              obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2,
+              obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 0.7, 1,
               0);
             int max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
             max_width = std::max(max_width, (int)i.w + 2);
-            // max_width = std::max(max_width, 283);
-            std::string coords_3d;
-            if (!std::isnan(i.z_3d)) {
-                std::stringstream ss;
-                ss << std::fixed << std::setprecision(2)
-                   << "x:" << i.x_3d << "m y:" << i.y_3d
-                   << "m z:" << i.z_3d << "m ";
-                coords_3d = ss.str();
-                cv::Size const text_size_3d = getTextSize(
-                  ss.str(), cv::FONT_HERSHEY_COMPLEX_SMALL,
-                  0.8, 1, 0);
-                int const max_width_3d =
-                  (text_size_3d.width > i.w + 2) ? text_size_3d.width : (i.w + 2);
-                if (max_width_3d > max_width)
-                    max_width = max_width_3d;
-            }
-
             cv::rectangle(
               mat_img,
               cv::Point2f(std::max((int)i.x - 1, 0),
