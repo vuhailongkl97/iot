ddc3c4ddbdeead4101a7451b71b185f533fe6c3d
long.vu <vuhailongkl97@gmail.com>
2022-12-16 22:54:03 +0700

    fix make `prepareLog` to be protected

--- libs/services/include/monitor.h
+++ libs/services/include/monitor.h
@@ -6,46 +6,37 @@
 #include <config_mgr.h>
 #include <server.h>
 
-enum class Jetsonhardware : int
-{
-    jA0 = 0,
-    jCPU,
-    jGPU,
-    jEND
-};
+enum class Jetsonhardware : int { jA0 = 0, jCPU, jGPU, jEND };
 
-class HardwareManager
-{
+class HardwareManager {
 protected:
     Logger& l;
     Config& cfg;
     Interface& inf;
     std::vector<float> m_temp_thresholds;
     std::string m_name;
+    virtual void prepareLog();
 
 public:
     HardwareManager(std::string name, std::vector<float> thresholds, Logger& _l,
                     Config& c, Interface& i) :
       l(_l),
-      cfg(c), inf(i), m_temp_thresholds(thresholds), m_name(name)
-    {}
+      cfg(c), inf(i), m_temp_thresholds(thresholds), m_name(name) {}
     virtual int getAvailableMem() = 0;
     virtual std::vector<float> getTemperatures() = 0;
 
-    void prepareLog();
     void monitor();
+	virtual ~HardwareManager() = default;
 };
 
-class Jetson : public HardwareManager
-{
+class Jetson : public HardwareManager {
 private:
     float getTemperature(Jetsonhardware);
 
 public:
     Jetson(std::string name, std::vector<float> thresholds, Logger& _l,
            Config& c, Interface& i) :
-      HardwareManager(name, thresholds, _l, c, i)
-    {}
+      HardwareManager(name, thresholds, _l, c, i) {}
     int getAvailableMem() override;
     std::vector<float> getTemperatures() override;
 };
